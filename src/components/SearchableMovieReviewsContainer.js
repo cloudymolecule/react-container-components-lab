9ba761d83500f8d1c2d926e8998ceaf6d9dc1e50
Cloudymolecule <baires82@gmail.com>
2021-04-28 21:57:24 -0400

    commit tree 1cdfd0dafea9689e3382f5137d92fbc114dcef92

--- src/components/SearchableMovieReviewsContainer.js
+++ src/components/SearchableMovieReviewsContainer.js
@@ -6,4 +6,33 @@ const NYT_API_KEY = 'dGpQ5OmGP2SgfvZimlpCUoF4iOag9qzZ';
 const URL = 'https://api.nytimes.com/svc/movies/v2/reviews/search.json?'
             + `api-key=${NYT_API_KEY}`;
 
+
 // Code SearchableMovieReviewsContainer Here
+export default class SearchableMovieReviewsContainer extends Component {
+    
+    state = {
+        reviews: [],
+        searchTerm: ''
+    }
+
+    render() {
+        return (
+            <form onSubmit={this.handleSubmission} className='searchable-movie-reviews'>
+                <input onChange={this.handleChange}></input>
+                <MovieReviews reviews={this.state.reviews}/>
+            </form>
+        )
+    }
+
+    handleChange = (e) => {
+        this.setState({searchTerm: e.target.value})
+    }
+
+    handleSubmission = (e) => {
+        e.preventDefault()
+        fetch(URL + `&query=${this.state.searchTerm}`).then(res => res.json()).then(data => this.setState({reviews: data.results}))
+    }
+
+}
+
+
