d11b26fd17a3ee6bd8a92453e43b307f3b915185
Cloudymolecule <baires82@gmail.com>
2021-04-28 21:57:31 -0400

    Done.

--- src/components/MovieReviews.js
+++ src/components/MovieReviews.js
@@ -1,1 +1,9 @@
 // Code MovieReviews Here
+import React from 'react'
+
+const MovieReviews = props => <div className='review-list'>{
+    props.reviews.map(review => <p key={review.display_title + Math.floor(Math.random() * 10) + Math.floor(Math.random() * 10)} className='review'>{review.display_title}</p>)
+}</div>
+
+export default MovieReviews
+
